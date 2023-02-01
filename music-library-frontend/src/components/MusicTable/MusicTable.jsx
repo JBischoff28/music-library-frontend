b5987b2cf9dc6e7c5a2b80c391e0483c1c5b86e4
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-02-01 16:37:44 -0500

    Finished Creating The MusicTable That Will Serve To Display All Songs In The Database In The Form Of A Table.

--- music-library-frontend/src/components/MusicTable/MusicTable.jsx
+++ music-library-frontend/src/components/MusicTable/MusicTable.jsx
@@ -2,14 +2,30 @@
 const MusicTable = (props) => {
     
     return ( 
-        <ul>
-            {props.songsArray.map((song, index) =>
-                (
-                <div key={index}>
-                </div>
-                )
-            )}
-        </ul>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                    <th>Release Date</th>
+                    <th>Genre</th>
+                </tr>
+            </thead>
+            <tbody>
+                {props.songsArray.map((song, index) =>
+                    (
+                    <tr key={index}>
+                        <td>{song.title}</td>
+                        <td>{song.artist}</td>
+                        <td>{song.album}</td>
+                        <td>{song.release_date}</td>
+                        <td>{song.genre}</td>
+                    </tr>
+                    )
+                )}
+            </tbody>
+        </table>
      );
 }
  
