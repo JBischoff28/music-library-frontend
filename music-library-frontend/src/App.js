fd3c4d72f0ede0fd09e692550054a6451af1d4c2
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-02-01 12:25:44 -0500

    Installed And Imported Axios. Created An Async Function That Gets All Songs In The Database And Stores The Promise In A Response Variable. Created A State Varible And Function That Will Be Responsible For Holding And Updating The Response Var. Created A useEffect That Is Responsible For Running The Async Function Upon Page Start.

--- music-library-frontend/src/App.js
+++ music-library-frontend/src/App.js
@@ -1,6 +1,20 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    GetAllSongs();
+  }, []);
+
+  async function GetAllSongs() {
+    const response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log(response.data); // Console logged in order to debug when needed
+    setSongs(response.data);
+  }
+
   return (
     <div className="App">
 
