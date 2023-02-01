70b8dba3517be1eb5ae6078337bd9b8c69662003
Joshua Bischoff <joshuabischoff1@gmail.com>
2023-02-01 17:10:58 -0500

    Began Development Of NewSongForm Component. Ignore Manifest Readd And MusicIcon.

---
+++ music-library-frontend/src/components/NewSongForm/NewSongForm.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const NewSongForm = (props) => {
+
+    const [newSong, setNewSong] = useState({});
+
+    return ( 
+        <p></p>
+     );
+}
+ 
+export default NewSongForm;
